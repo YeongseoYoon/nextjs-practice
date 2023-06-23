360f51e07332c6f9e17626e5ff9201f3419ab25c
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-06-23 19:28:57 +0900

    :lipstick: Detail 페이지 스타일링
    detail 페이지 스타일링

--- pages/list/[id]/index.tsx
+++ pages/list/[id]/index.tsx
@@ -3,6 +3,7 @@ import Seo from "../../../components/seo/Seo";
 import { getBooks } from "../../../apis/api";
 import { IBook, IBookResponse } from "@/types/types";
 
+import styles from "./List.module.css";
 interface DetailPageProps {
   bookList: IBookResponse;
 }
@@ -28,17 +29,31 @@ export const getServerSideProps: GetServerSideProps<DetailPageProps> = async ({
 
 export default function DetailPage({ bookList }: DetailPageProps) {
   return (
-    <div>
+    <main className={styles.main}>
       <Seo title={`Detail - ${bookList.list_name}`} />
-      {!bookList ? (
-        <h4>Loading...</h4>
-      ) : (
-        bookList?.books.map((book: IBook) => (
-          <div key={`${book.amazon_product_url}-${book.primary_isbn13}`}>
-            <h4>{book.title}</h4>
-          </div>
-        ))
-      )}
-    </div>
+      <h1 className={styles.detail_title}>{bookList.list_name}</h1>
+      <div className={styles.detail_wrapper}>
+        {!bookList ? (
+          <h4>Loading...</h4>
+        ) : (
+          bookList?.books.map((book: IBook) => (
+            <div
+              className={styles.detail_card}
+              key={`${book.amazon_product_url}-${book.primary_isbn13}`}
+            >
+              <img
+                className={styles.detail_image}
+                src={`${book.book_image}`}
+              ></img>
+              <h3 className={styles.detail_bookTitle}>{book.title}</h3>
+              <h4 className={styles.detail_author}>{book.author}</h4>
+              <button>
+                <a href={`${book.amazon_product_url}`}>Buy Now →</a>
+              </button>
+            </div>
+          ))
+        )}
+      </div>
+    </main>
   );
 }
